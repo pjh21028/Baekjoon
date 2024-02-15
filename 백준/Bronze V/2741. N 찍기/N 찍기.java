4bf8345a4bae112e46c865bf35f052a6b8f967c3
pjh21028 <157588041+pjh21028@users.noreply.github.com>
2024-02-15 23:27:31 +0900

    [Bronze V] Title: N 찍기, Time: 1068 ms, Memory: 31356 KB -BaekjoonHub

---
+++ 백준/Bronze V/2741. N 찍기/N 찍기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {		
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int iNum;
+        iNum = Integer.parseInt(br.readLine());
+
+		for(int i = 1; i <= iNum; i++) {
+			System.out.println(i);
+		}
+	}
+}
